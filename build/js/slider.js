957edbfef5c585bb1ecadad3dea17e7df8f7066d
Ibrahim <antonutkin777@gmail.com>
2020-06-20 23:03:13 +0300

    Добавляет слайдеры на главную страницу и страницу карточки товара

--- build/js/slider.js
+++ build/js/slider.js
@@ -8,4 +8,20 @@
       speed: 1000,
     });
   });
+
+  $(document).ready(function(){
+    $('.slider__additive').slick({
+      arrows: true,
+      slidesToScroll: 4,
+      slidesToShow: 4,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.slider__popular').slick({
+      arrows: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+    });
+  });
 })();
