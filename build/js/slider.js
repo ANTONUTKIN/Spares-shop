363c69fdc045bc4c9c71b7349b9ef496ae77ab34
Ibrahim <antonutkin777@gmail.com>
2020-07-01 17:41:05 +0300

    Добавляет адаптив планшетной ширины на главную старницу

--- build/js/slider.js
+++ build/js/slider.js
@@ -1,11 +1,23 @@
 'use strict';
 
 (function () {
-
+//
   $(document).ready(function(){
     $('.slider').slick({
       dots: true,
       speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      responsive: [
+        {
+          breakpoint: 1340,
+          settings: {
+            arrows: false
+          }
+        }
+      ]
     });
   });
 
@@ -14,6 +26,17 @@
       arrows: true,
       slidesToScroll: 4,
       slidesToShow: 4,
+      responsive: [
+        {
+          breakpoint: 1340,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        }
+      ]
     });
   });
 
@@ -22,6 +45,47 @@
       arrows: true,
       slidesToShow: 4,
       slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1340,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        }
+      ]
+    });
+  });
+
+  $(document).ready(function(){
+    $('.slider__banner').slick({
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1340,
+          settings: {
+            slidesToShow: 2,
+            dots: true,
+          }
+        }
+      ]
+    });
+  });
+
+  $(document).ready(function(){
+    $('.product-search__input:first').show();
+    $('.product-search__list li:first').addClass('product-search__button--active');
+
+    $('.product-search__list li').click(function(event) {
+      var index = $(this).index();
+      $('.product-search__list li').removeClass('product-search__button--active');
+      $(this).addClass('product-search__button--active');
+      $('.product-search__input').hide();
+      $('.product-search__input').eq(index).show();
     });
   });
 })();
